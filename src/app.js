b49a10b6e56e5c4a082481b6ed3ee76877c56bac
Guilherme Inoe <guilhermeinoe82@gmail.com>
2026-04-13 14:51:36 -0300

    feat: inicializando o ambiente

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+const usuarioRoutes = require('./routes/usuarioRoutes');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Documentação
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Rota Principal
+app.get('/', (req, res) => {
+    res.redirect('/api-docs');
+});
+
+// Rotas da Aplicação
+app.use('/usuarios', usuarioRoutes);
+
+module.exports = app;
