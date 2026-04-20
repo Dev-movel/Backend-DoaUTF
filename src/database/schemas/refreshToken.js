9f82e405f78f6b3d6e9c24d6e5079480da6162d7
Guilherme Inoe <guilhermeinoe82@gmail.com>
2026-04-20 15:02:46 -0300

    feat:

--- src/database/schemas/refreshToken.js
+++ src/database/schemas/refreshToken.js
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS refresh_token (
   id SERIAL PRIMARY KEY,
   pessoa_id INTEGER NOT NULL,
   token_hash TEXT NOT NULL,
-  expiracao TIMESTAMP NOT NULL,
-  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  expires_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT fk_pessoa
     FOREIGN KEY (pessoa_id)
     REFERENCES pessoa(id)
