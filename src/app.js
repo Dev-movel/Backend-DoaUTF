2f4eb3831b88bb197d85b0f93f249642cd98236f
MagalhaesExe <alexmagalhaesjr15@gmail.com>
2026-06-21 14:49:32 -0300

    feat: cria rotas e controller para submissão de avaliações

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const authRoutes = require('./routes/authRoutes');
 const itemRoutes = require('./routes/itemRoutes');
 const categoriaRoutes = require('./routes/categoriaRoutes');
 const solicitacaoRoutes = require('./routes/solicitacaoRoutes');
+const avaliacaoRoutes = require('./routes/avaliacaoRoutes');
 
 const app = express();
 
@@ -32,5 +33,6 @@ app.use('/usuarios', usuarioRoutes);
 app.use('/itens', itemRoutes);
 app.use('/categorias', categoriaRoutes);
 app.use('/solicitacoes', solicitacaoRoutes);
+app.use('/avaliacao', avaliacaoRoutes);
 
 module.exports = app;
